2f0b98f6cb8300fac3b67cc84996ea4ead5ff3ca
Paul-Mackenzie-Mitchell <paul.mackenzie.mitchell@gmail.com>
2022-01-05 12:46:13 +0000

    Updated calculator

--- RB_101/lesson_2/calculator.rb
+++ RB_101/lesson_2/calculator.rb
@@ -1,43 +1,76 @@
-answer =nil
-num_1 = nil
-num_2 = nil
-while answer == nil
-test_array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-#ask the user for two numbers
-  
+def prompt(message) #adds formatting
+  puts "=> #{message}"
+end
 
-  while test_array.include?(num_1) == false
-    puts "Please enter your first of two numbers to perform an operation on: "
-    num_1 = gets.chomp
-  end
-  while test_array.include?(num_2) == false
-    puts "Please enter your second of two numbers to perform an operation on: "
-    num_2 = gets.chomp
-  end
-
-
-#ask the user for an operation to perform
-  puts "Please enter an operation to perform ( +, -, *, /)"
-  operation = gets.chomp.to_s
-#perform the operation on the numbers
-
-  if operation == "+"
-    answer = num_1 + num_2
-  elsif operation == "-"
-    answer = num_1 - num_2
-  elsif operation == "*"
-    answer = num_1 * num_2
-  elsif operation == "/"
-    num_1 = num_1.to_f
-    num_2 = num_2.to_f
-    while num_2 == 0.0
-      puts "Cannot divide by 0, please enter a valid denominator"
-      num_2 = gets.chomp.to_f
+def test_num #Method to test if number is an integer 0..10
+  test_array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  num = nil
+  loop do
+    prompt("Please enter an integer: ")
+    num = gets.chomp
+    if test_array.include?(num) == false
+      prompt("Please enter a valid number")
+    else
+      num
+      break
     end
-    answer = num_1 / num_2
-  else
-    puts "Your input operation is invalid"
+  end
+  num
+end
+
+def test_operator
+  test_array = ['+', '-', '*', '/']
+  op = nil
+  loop do 
+    prompt("Please enter an operation to perform ( +, -, *, /)")
+    op = gets.chomp
+    if test_array.include?(op) == false
+      prompt("Please enter a valid operation")
+    else
+      return op
+      break
+    end
   end
 end
-#outut the results
-puts "#{num_1} #{operation} #{num_2} is #{answer}"
+
+
+prompt("Welcome to Calculator")
+
+num_1 = nil
+num_2 = nil
+operation = nil
+
+loop do 
+  #ask the user for two numbers
+  num_1 = test_num
+  num_2 = test_num
+  
+  #perform the operation on the numbers
+  
+  #ask the user for an operation to perform
+  operation = test_operator
+  
+  answer = case operation
+            when '+'
+              num_1.to_i + num_2.to_i
+            when '-'
+              num_1.to_i - num_2.to_i
+            when '*'
+              num_1.to_i * num_2.to_i
+            when '/'
+              while num_2 == '0'
+                prompt("Cannot divide by 0, please enter a valid denominator")
+                num_2 = gets.chomp
+              end
+              num_1.to_f / num_2.to_f
+  end              
+    
+  #outut the results
+  prompt("#{num_1} #{operation} #{num_2} is #{answer}")
+  prompt("Enter 'Y' if you like to perform another calculation?")
+  continue = gets.chomp.downcase
+  if continue != 'y'
+    prompt("Thank you for using the calcuator")
+    break
+  end
+end
