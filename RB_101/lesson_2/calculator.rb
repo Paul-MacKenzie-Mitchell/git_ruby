0789b00d5c2d0f8932aacb1a31c72172470b9eb6
Paul-Mackenzie-Mitchell <paul.mackenzie.mitchell@gmail.com>
2022-01-06 03:07:57 +0000

    calculator and ls_calculator post Rubocop

--- RB_101/lesson_2/calculator.rb
+++ RB_101/lesson_2/calculator.rb
@@ -1,19 +1,18 @@
-def prompt(message) #adds formatting
+
+def prompt(message)
   puts "=> #{message}"
 end
 
-def test_num #Method to test if number is an integer 0..10
-  test_array = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# Method to test if number is an integer 0..10
+def test_num
+  test_array = %w[0 1 2 3 4 5 6 7 8 9]
   num = nil
   loop do
-    prompt("Please enter an integer: ")
+    prompt('Please enter an integer: ')
     num = gets.chomp
-    if test_array.include?(num) == false
-      prompt("Please enter a valid number")
-    else
-      num
-      break
-    end
+    break unless test_array.include?(num) == false
+
+    prompt('That was not a valid number')
   end
   num
 end
@@ -21,56 +20,55 @@ end
 def test_operator
   test_array = ['+', '-', '*', '/']
   op = nil
-  loop do 
-    prompt("Please enter an operation to perform ( +, -, *, /)")
+  loop do
+    prompt('Please enter an operation to perform ( +, -, *, /)')
     op = gets.chomp
     if test_array.include?(op) == false
-      prompt("Please enter a valid operation")
+      prompt('Please enter a valid operation')
     else
       return op
-      break
     end
   end
 end
 
+prompt('Welcome to Calculator')
 
-prompt("Welcome to Calculator")
-
-num_1 = nil
-num_2 = nil
+num1 = nil
+num2 = nil
 operation = nil
 
-loop do 
-  #ask the user for two numbers
-  num_1 = test_num
-  num_2 = test_num
-  
-  #perform the operation on the numbers
-  
-  #ask the user for an operation to perform
+loop do
+  # ask the user for two numbers
+  num1 = test_num
+  num2 = test_num
+
+  # perform the operation on the numbers
+
+  # ask the user for an operation to perform
   operation = test_operator
-  
+
   answer = case operation
-            when '+'
-              num_1.to_i + num_2.to_i
-            when '-'
-              num_1.to_i - num_2.to_i
-            when '*'
-              num_1.to_i * num_2.to_i
-            when '/'
-              while num_2 == '0'
-                prompt("Cannot divide by 0, please enter a valid denominator")
-                num_2 = gets.chomp
-              end
-              num_1.to_f / num_2.to_f
-  end              
-    
-  #outut the results
-  prompt("#{num_1} #{operation} #{num_2} is #{answer}")
+           when '+'
+             num1.to_i + num2.to_i
+           when '-'
+             num1.to_i - num2.to_i
+           when '*'
+             num1.to_i * num2.to_i
+           when '/'
+             while num2 == '0'
+               prompt('Cannot divide by 0, please enter a valid denominator')
+               num2 = gets.chomp
+               num2 = test_num
+             end
+             num1.to_i / num2.to_f
+           end
+
+  # outut the results
+  prompt("#{num1} #{operation} #{num2} is #{answer}")
   prompt("Enter 'Y' if you like to perform another calculation?")
   continue = gets.chomp.downcase
   if continue != 'y'
-    prompt("Thank you for using the calcuator")
+    prompt('Thank you for using the calcuator')
     break
   end
 end
--- RB_101/lesson_2/ls_calculator.rb
+++ RB_101/lesson_2/ls_calculator.rb
@@ -1,4 +1,4 @@
-def prompt(message) #adds formatting
+def prompt(message)
   puts "=> #{message}"
 end
 
@@ -6,31 +6,30 @@ def valid_number?(num)
   num.to_i != 0
 end
 
-def operation_to_message(op)
-  case op
-    when '1'
-      'Adding'
-    when '2'
-      'Subtracting'
-    when '3'
-      'Multipling'
-    when '4'
-      'Dividing'
+def operation_to_message(oper)
+  case oper
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multipling'
+  when '4'
+    'Dividing'
   end
 end
-
-prompt("Welcome to Calculator. Please enter your name.")
+prompt('Welcome to Calculator. Please enter your name.')
 name = ''
 loop do
   name = gets.chomp
-  if name.empty?()
-    prompt("Make sure to enter a valid name.")
+  if name.empty?
+    prompt('Make sure to enter a valid name.')
   else
     break
   end
 end
 
-loop do 
+loop do
   number1 = ''
   loop do
     prompt("What's the first number")
@@ -38,53 +37,52 @@ loop do
     if valid_number?(number1)
       break
     else
-      prompt("Enter valid number")
+      prompt('Enter valid number')
     end
   end
-  
+
   number2 = ''
-  loop do 
+  loop do
     prompt("What's the second number")
     number2 = gets.chomp
     if valid_number?(number2)
       break
     else
-      prompt("Enter valid number")
+      prompt('Enter valid number')
     end
   end
-  
-  operator_prompt = <<-MSG 
+
+  operator_prompt = <<-MSG
   What operation would you like to perform
-      1). Add 
+      1). Add#{' '}
       2). Subtract
-      3). multiply 
+      3). multiply#{' '}
       4). divide
   MSG
   prompt(operator_prompt)
   operator = ''
   loop do
     operator = gets.chomp
-    if %w(1 2 3 4).include?(operator)
+    if %w[1 2 3 4].include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3 or 4.")
+      prompt('Must choose 1, 2, 3 or 4.')
     end
   end
-  
+
   prompt("#{operation_to_message(operator)} the two numbers...")
 
-
   result = case operator
-            when '1'
-              number1.to_i + number2.to_i
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              number1.to_i * number2.to_i
-            when '4'
-              number1.to_f / number2.to_f
-  end
-  
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2
+           end
+
   prompt("The result is #{result}")
   prompt("Do you want to perform another calculation #{name}? (Y to calculate again)")
   answer = gets.chomp
