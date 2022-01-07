b705800082ea34e69344d7d31b5e96ae73ee58cc
Paul-Mackenzie-Mitchell <paul.mackenzie.mitchell@gmail.com>
2022-01-07 00:34:58 +0000

    Finished calculator and yml message file

--- RB_101/lesson_2/ls_calculator.rb
+++ RB_101/lesson_2/ls_calculator.rb
@@ -1,13 +1,33 @@
+require 'yaml'
+MESSAGES = YAML.load_file('ls_calculator_messages.yml')
+
+
 def prompt(message)
   puts "=> #{message}"
 end
 
+def integer?(num)
+  num.to_i.to_s == num
+end
+
+def float?(num)
+  num.to_f.to_s ==num
+end
+
 def valid_number?(num)
-  num.to_i != 0
+  integer?(num) || float?(num)
+end
+
+def use_float(num)
+  if integer?(num)
+    num.to_i
+  else
+    num.to_f
+  end
 end
 
 def operation_to_message(oper)
-  case oper
+  operator = case oper
   when '1'
     'Adding'
   when '2'
@@ -17,13 +37,15 @@ def operation_to_message(oper)
   when '4'
     'Dividing'
   end
+  operator
 end
-prompt('Welcome to Calculator. Please enter your name.')
+
+prompt(MESSAGES['welcome'])
 name = ''
 loop do
   name = gets.chomp
   if name.empty?
-    prompt('Make sure to enter a valid name.')
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
@@ -32,31 +54,34 @@ end
 loop do
   number1 = ''
   loop do
-    prompt("What's the first number")
+    prompt(MESSAGES['first_num'])
     number1 = gets.chomp
     if valid_number?(number1)
       break
     else
-      prompt('Enter valid number')
+      prompt(MESSAGES['valid_num'])
     end
   end
 
   number2 = ''
   loop do
-    prompt("What's the second number")
+    prompt(MESSAGES['second_num'])
     number2 = gets.chomp
     if valid_number?(number2)
       break
     else
-      prompt('Enter valid number')
+      prompt(MESSAGES['valid_num'])
     end
   end
 
+  number1 = use_float(number1)
+  number2 = use_float(number2)
+  
   operator_prompt = <<-MSG
   What operation would you like to perform
-      1). Add#{' '}
+      1). Add
       2). Subtract
-      3). multiply#{' '}
+      3). multiply
       4). divide
   MSG
   prompt(operator_prompt)
@@ -66,23 +91,27 @@ loop do
     if %w[1 2 3 4].include?(operator)
       break
     else
-      prompt('Must choose 1, 2, 3 or 4.')
+      prompt(MESSAGES['must_choose'])
     end
   end
 
-  prompt("#{operation_to_message(operator)} the two numbers...")
-
   result = case operator
-           when '1'
-             number1.to_i + number2.to_i
-           when '2'
-             number1.to_i - number2.to_i
-           when '3'
-             number1.to_i * number2.to_i
-           when '4'
-             number1.to_f / number2
-           end
+          when '1'
+            number1 + number2
+          when '2'
+            number1 - number2
+          when '3'
+            number1 * number2
+          when '4'
+            while number2 == 0 || number2 == 0.0
+               prompt(MESSAGES['zero_error'])
+               number2 = gets.chomp
+               number2 = use_float(number2)
+             end
+            number1.to_f / number2
+          end
 
+  prompt("#{operation_to_message(operator)} the two numbers...")
   prompt("The result is #{result}")
   prompt("Do you want to perform another calculation #{name}? (Y to calculate again)")
   answer = gets.chomp
