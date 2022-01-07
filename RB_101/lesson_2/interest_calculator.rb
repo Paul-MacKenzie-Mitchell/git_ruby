c724e136ddda761b407a31fc9bbb1c8e6d67d00a
Paul-Mackenzie-Mitchell <paul.mackenzie.mitchell@gmail.com>
2022-01-07 02:53:31 +0000

    Finished calc

--- RB_101/lesson_2/interest_calculator.rb
+++ RB_101/lesson_2/interest_calculator.rb
@@ -23,7 +23,17 @@ def input_valid?(input)
       prompt('Please enter a valid number')
     end
   end
+  input.to_f
 end
+
+def use_float(num)
+  if integer?(num)
+    num.to_i
+  else
+    num.to_f
+  end
+end
+
 
 prompt('Welcome to the interest calculator')
 prompt('Please enter a user name')
@@ -48,6 +58,28 @@ loop do
   #input for loan length
   prompt('Enter the number of months the loan will be payed in')
   months = input_valid?(months)
+  #calculate
+  monthly_interest_rate = annual_percentage_rate / 12
+  if months != 0.0
+    monthly_payment = loan_amount * (monthly_interest_rate/(1 - (1 + monthly_interest_rate)**(-months)))
+  else
+    monthly_payment = loan_amount
+  end
+  
+  if months != 0.0
+    total_interest = (monthly_payment * months) - loan_amount
+  else
+    total_interest = 0.0
+  end
+  
+  #output
+  if months == 0.0
+    prompt("#{name}, your monthly payments for your loan of $#{'%.2f' % loan_amount} will be $#{'%.2f' % monthly_payment} paid immediately")
+    prompt("The total interest you will pay is $#{'%.2f' % total_interest}")
+  else
+    prompt("#{name}, your monthly payments for your loan of $#{'%.2f' % loan_amount} will be $#{'%.2f' % monthly_payment} for #{months.to_i.to_s} months")
+    prompt("The total interest you will pay is $#{'%.2f' % total_interest}")
+  end
   #calculate again?
   prompt "Would You like to calculate another loan #{name}?  Enter Y to continue"
   continue = gets.chomp
