f596b35e746d1ade385627a2642546f2cdaada07
Paul-Mackenzie-Mitchell <paul.mackenzie.mitchell@gmail.com>
2022-03-11 02:26:09 +0000

    working 5 round game

--- RB_101/lesson_6/21.rb
+++ RB_101/lesson_6/21.rb
@@ -1,23 +1,36 @@
 require 'pry'
+require 'yaml'
 
-DECK = { 'Spades': [ {'2': 2}, {'3': 3}, {'4': 4}, {'5': 5}, {'6': 6}, {'7': 7}, {'8': 8}, {'9': 9}, 
-                   {'10': 10}, {'Jack': 10}, {'Queen': 10}, {'King': 10}, {'Ace': 11} ],
-        'Clubs': [ {'2': 2}, {'3': 3}, {'4': 4}, {'5': 5}, {'6': 6}, {'7': 7}, {'8': 8}, {'9': 9}, 
-                  {'10': 10}, {'Jack': 10}, {'Queen': 10}, {'King': 10}, {'Ace': 11} ],
-        'Hearts': [ {'2': 2}, {'3': 3}, {'4': 4}, {'5': 5}, {'6': 6}, {'7': 7}, {'8': 8}, {'9': 9}, 
-                  {'10': 10}, {'Jack': 10}, {'Queen': 10}, {'King': 10}, {'Ace': 11} ],
-        'Diamonds': [ {'2': 2}, {'3': 3}, {'4': 4}, {'5': 5}, {'6': 6}, {'7': 7}, {'8': 8}, {'9': 9}, 
-                  {'10': 10}, {'Jack': 10}, {'Queen': 10}, {'King': 10}, {'Ace': 11} ]
-        }
-
-HIT_OR_STAY = %w[HIT STAY H S]
+MESSAGES = YAML.load_file('21_messages.yml')
+DECK = { 'Spades': [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 },
+                    { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 },
+                    { '10': 10 }, { 'Jack': 10 }, { 'Queen': 10 },
+                    { 'King': 10 }, { 'Ace': 11 }],
+         'Clubs': [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 },
+                   { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 },
+                   { '10': 10 }, { 'Jack': 10 }, { 'Queen': 10 },
+                   { 'King': 10 }, { 'Ace': 11 }],
+         'Hearts': [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 },
+                    { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 },
+                    { '10': 10 }, { 'Jack': 10 }, { 'Queen': 10 },
+                    { 'King': 10 }, { 'Ace': 11 }],
+         'Diamonds': [{ '2': 2 }, { '3': 3 }, { '4': 4 }, { '5': 5 },
+                      { '6': 6 }, { '7': 7 }, { '8': 8 }, { '9': 9 },
+                      { '10': 10 }, { 'Jack': 10 }, { 'Queen': 10 },
+                      { 'King': 10 }, { 'Ace': 11 }] }
+MAX_PLAYABLE_VALUE = 21
+DEALER_LIMIT = 17
+CHOICE = %w(HIT STAY H S)
+HIT = %w(HIT H)
+STAY = %w(STAY S)
 
 # =======================================
 #  Initialization Methods
 # =======================================
 
 def initialize_deck
-  DECK.clone
+  system 'clear'
+  Marshal.load( Marshal.dump(DECK))
 end
 
 # =======================================
@@ -28,22 +41,52 @@ def prompt(message)
   puts "=> #{message}"
 end
 
+def welcome
+  puts "            <-<-<->->->     #{MESSAGES['welcome']}     <-<-<->->->"
+  puts ""
+  puts ""
+  prompt("If you would like to learn the rules of the game type 'rules'")
+  prompt("Otherwise hit any key to continue")
+  rules
+end
+
+def hit_any_key_to_continue
+  prompt("Hit any key to continue")
+  gets.chomp
+end
+
+def rules
+  choice = gets.chomp.downcase
+  if choice == 'rules' || choice == 'r'
+    puts MESSAGES['rules']
+    sleep 2.5
+    puts ""
+    hit_any_key_to_continue
+  end
+  system 'clear'
+  prompt("The first player to win 5 games takes the match!")
+  sleep 1
+end
+
 def convert_hand_to_string(hand)
   cards = []
-  hand.each do |array| 
+  hand.each do |array|
     cards << "the #{array[0].keys[0]} of #{array[1]}"
   end
   cards
 end
 
-def display_player_hand(hand, ender = '.', punct = ', ', joiner = ' and ')
+def display_player_hand(hand)
   string = 'Your hand inludes '
   cards = convert_hand_to_string(hand)
   cards.each do |card|
-    string += card + ender if card == cards[-1]
-    string += card + joiner if card == cards[-2]
-    string += card + punct if card != cards[-1] &&
-                              card != cards[-2]
+    string += if card == cards[-1]
+                card + '.'
+              elsif card == cards[-2]
+                card + ' and '
+              else
+                card + ', '
+              end
   end
   prompt(string)
   prompt("Your hand is worth #{hand_value(hand)} points.")
@@ -51,58 +94,93 @@ def display_player_hand(hand, ender = '.', punct = ', ', joiner = ' and ')
   puts ""
 end
 
-def display_dealer_hand(hand, ender = '.', punct = ', ', joiner = ' and ')
-  unknown = "an unknown card"
+def format_display_dealer_hand(hand, hide_card)
   string = 'The dealers hand inludes '
   cards = convert_hand_to_string(hand)
+  variable = "the hole card" if hide_card
+  variable = cards[0] if !hide_card
   cards.each do |card|
-    string += unknown if card == cards[0]
-    string += joiner + card + ender if card == cards[-1]
-    string += punct + card if card == cards[-2] &&
-                               card != cards[0]
-    string += punct + card if card != cards[-1] &&
-                              card != cards[-2] &&
-                              card != cards[0]
+    string += if card == cards[0]
+                variable
+              elsif card == cards[-1]
+                ' and ' + card + '.'
+              else
+                ', ' + card
+              end
   end
+  display_dealer_hand(hand, hide_card, string)
+end
+
+def display_dealer_hand(hand, hide_card, string)
   prompt(string)
+  prompt(MESSAGES['dealer'] + "#{hand_value(hand)} points.") if !hide_card
   prompt("The dealer busted!") if bust?(hand)
   puts ""
 end
 
-def display_winner(p_hand, d_hand)
-  winner = who_won?(p_hand, d_hand)
+def display_winner(winner)
   prompt("You Won!") if winner == "Player"
-  prompt("The Dealer Won...") if winner =="Dealer"
+  prompt("The Dealer Won...") if winner == "Dealer"
   prompt("It was a tie...") if winner == "Tie"
+end
+
+def score(player, dealer, draw)
+  puts ""
+  puts "         Match Score"
+  puts '==========================='
+  puts " Player | Dealer | Ties "
+  puts "========+=================="
+  puts "   #{player}    |    #{dealer}  |   #{draw}"
+  puts ""
+  hit_any_key_to_continue
+  system 'clear'
 end
 # =======================================
 # Game Choice Methods
 # =======================================
 
-def deal_card_to_player?(deck, hand)
-  answer = ''
+def ask_to_deal_card(answer)
+  prompt("Would you like to hit or stay?")
   loop do
-    prompt("Would you like to hit or stay?")
-    loop do
-      answer = gets.chomp.upcase
-      break if HIT_OR_STAY.include?(answer)
-      prompt("Please enter a valid answer...")
-    end
-    deal_one_card(deck, hand) if answer == "HIT" || answer == "H"
-    break if answer == "STAY" || answer == "S"
-    display_player_hand(hand)
-    break if bust?(hand)
+    answer = gets.chomp.upcase
+    break if HIT.include?(answer) || STAY.include?(answer)
+    prompt("Please enter a valid answer...")
   end
   answer
 end
 
-def deal_card_to_dealer?(deck, d_hand, p_hand)
+def deal_card_to_player?(deck, hand)
+  answer = ''
+  loop do
+    answer = ask_to_deal_card(answer)
+    deal_one_card(deck, hand) if HIT.include?(answer)
+    display_player_hand(hand)
+    chose_to_stay if STAY.include?(answer)
+    break if STAY.include?(answer) || bust?(hand)
+  end
+end
+
+def deal_card_to_dealer?(deck, d_hand, p_hand, hide_card)
   loop do
     break if bust?(p_hand)
-    break if hand_value(d_hand) >= 17
+    prompt("The Dealer stays") if  hand_value(d_hand) >= DEALER_LIMIT
+    break if hand_value(d_hand) >= DEALER_LIMIT
     deal_one_card(deck, d_hand)
   end
-  display_dealer_hand(d_hand)
+  format_display_dealer_hand(d_hand, hide_card)
+end
+
+def chose_to_stay
+  system 'clear'
+  prompt("You chose to stay...it is now the dealers turn")
+  sleep 1.5
+end
+
+def play_again?
+  valid = ['Y', 'y', 'Yes', 'YES', 'yes']
+  prompt("Would you like to play again? Y or N")
+  play_again = gets.chomp
+  valid.any?(play_again)
 end
 
 # =======================================
@@ -111,7 +189,8 @@ end
 
 def initial_deal(deck)
   cards = []
-  2.times do |card| card = select_card(deck)
+  2.times do
+    card = select_card(deck)
     cards << card
   end
   cards
@@ -125,7 +204,7 @@ def select_card(deck)
   deck[suite].delete(card[0])
   card = card[0], suite
 end
-  
+
 def deal_one_card(deck, hand)
   card = select_card(deck)
   hand << card
@@ -142,30 +221,28 @@ def hand_value(hand)
   hand.each do |card|
     value += card[0].values[0]
   end
-  return value if value <= 21
+  return value if value <= MAX_PLAYABLE_VALUE
   value -= 10 if check_for_ace(hand)
-  return value
+  value
 end
 
-def ace_values(hand)
-  
-end
+def ace_values(hand); end
 
 def check_for_ace(hand)
   ace = 0
   hand.each do |card|
-    ace += 1 if card[0] == {'Ace': 11}
+    ace += 1 if card[0] == { 'Ace': 11 }
   end
   return false if ace == 0
-  return ace 
+  ace
 end
-
 
 # =======================================
 # Game Win Methods
 # =======================================
+
 def bust?(hand)
-  return true if hand_value(hand) > 21
+  return true if hand_value(hand) > MAX_PLAYABLE_VALUE
   false
 end
 
@@ -177,16 +254,62 @@ def who_won?(p_hand, d_hand)
   return "Tie" if hand_value(d_hand) == hand_value(p_hand)
 end
 
+def add_win_count(winner, p_win, d_win, draw)
+  if winner == "Player"
+    p_win += 1
+  elsif winner == "Dealer"
+    d_win += 1
+  else
+    draw +=1
+  end
+  return p_win, d_win, draw
+end
+ 
 # =======================================
-# Game Play
+# Game Play Methods
 # =======================================
 
-current_deck = initialize_deck
-player_hand = initial_deal(current_deck)
-dealer_hand = initial_deal(current_deck)
-display_dealer_hand(dealer_hand)
-display_player_hand(player_hand)
-deal_card_to_player?(current_deck, player_hand)
-deal_card_to_dealer?(current_deck, dealer_hand, player_hand)
-display_winner(player_hand, dealer_hand)
-puts "Done!"
+def round(current_deck)
+  hide_card = true
+  # current_deck = initialize_deck
+  player_hand = initial_deal(current_deck)
+  dealer_hand = initial_deal(current_deck)
+  format_display_dealer_hand(dealer_hand, hide_card)
+  display_player_hand(player_hand)
+  deal_card_to_player?(current_deck, player_hand)
+  hide_card = false
+  deal_card_to_dealer?(current_deck, dealer_hand, player_hand, hide_card)
+  round_winner = who_won?(player_hand, dealer_hand)
+  display_winner(round_winner)
+  round_winner
+end
+
+# working on this.  Add a counter that retuns from round method
+def game
+  welcome
+  loop do 
+    player_win = 0
+    dealer_win = 0
+    tie = 0
+    loop do
+      current_deck = initialize_deck
+      round_winner = round(current_deck)
+      player_win, dealer_win, tie = add_win_count(round_winner, player_win, dealer_win, tie)
+      score(player_win, dealer_win, tie)
+      if player_win == 5
+        prompt("YOU WON THE MATCH!!!")
+        break
+      elsif dealer_win == 5
+        prompt("You lost the match...so sad :(")
+        break
+      end
+    end
+    break if !play_again?
+  end
+  prompt("Thanks for playing")
+end
+# =======================================
+# Game Play 
+# =======================================
+
+game
